2e81e8dbe4b72147b51120db6e6337a79e80f2c2
Eric Green <ericgreen.developer@gmail.com>
2017-07-20 11:22:12 -0400

    Add functions for adding customers and events

--- includes/functions.php
+++ includes/functions.php
@@ -2,19 +2,46 @@
 
 namespace cio;
 
+
 function update_customer( $id, $data = array() ) {
 
-	$url = API_ENDPOINT . $id;
+	$url = ltrim(API_ENDPOINT . $id, '/' );
+
+	$site_id = get_option( Options::SITE_ID );
+	$api_key = get_option( Options::SITE_ID );
 
 	$args = array(
-		'method' => 'put',
-		'body'   => $data
+		'method'  => 'PUT',
+		'body'    => $data,
+		'headers' => array(
+			'Authorization' => 'Basic ' . base64_encode( "$site_id:$api_key" )
+		)
 	);
 
-	return wp_remote_request( $url, $args );
+	$res = wp_remote_request( $url, $args );
+
+	return $res['response']['code'] === 200;
 
 }
 
+
 function customer_event( $customer_id, $name, $data = array() ) {
+
+	$url = trailingslashit( API_ENDPOINT . $customer_id ) . trim( $name, '/' );
+
+	$site_id = get_option( Options::SITE_ID );
+	$api_key = get_option( Options::SITE_ID );
+
+	$args = array(
+		'method'  => 'POST',
+		'body'    => $data,
+		'headers' => array(
+			'Authorization' => 'Basic ' . base64_encode( "$site_id:$api_key" )
+		)
+	);
+
+	$res = wp_remote_request( $url, $args );
+
+	return $res['response']['code'] === 200;
 
 }
