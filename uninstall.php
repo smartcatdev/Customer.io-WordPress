fa8482fec3ad33d186d01378541326b4c6049fd4
Eric Green <ericgreen.developer@gmail.com>
2017-07-25 16:34:07 -0400

    Delete settings on uninstall

--- uninstall.php
+++ uninstall.php
@@ -13,3 +13,12 @@ global $wpdb;
 
 
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}cio_events" );
+
+
+$options = new \ReflectionClass( Options::class );
+
+foreach ( $options->getConstants() as $option ) {
+
+	delete_option( $option );
+
+}
