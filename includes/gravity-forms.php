d79fade655b51f743bfe9f8e6afbf707a6dee2d8
Eric Green <ericgreen.developer@gmail.com>
2017-07-25 13:10:40 -0400

    Remove foreach when processing submit

--- includes/gravity-forms.php
+++ includes/gravity-forms.php
@@ -9,49 +9,46 @@ function process_events( $entry, $form ) {
 	);
 
 	$events = get_events( $args );
+	$event  = $events[0];
 
-	foreach ( $events as $event ) {
+	$customer   = array();
+	$event_data = array();
 
-		$customer   = array();
-		$event_data = array();
+	foreach ( $event['field_map'] as $gf_id => $map ) {
 
-		foreach ( $event['field_map'] as $gf_id => $map ) {
+		if ( $gf_id != $event['id_field'] && $gf_id != $event['email_field'] ) {
 
-			if ( $gf_id != $event['id_field'] && $gf_id != $event['email_field'] ) {
+			// Variable variable for data variable
+			$type = $map['type'] == 'customer' ? 'customer' : 'event_data';
 
-				// Variable variable for data variable
-				$type = $map['type'] == 'customer' ? 'customer' : 'event_data';
+			if ( is_array( $map['prop'] ) ) {
 
-				if ( is_array( $map['prop'] ) ) {
+				foreach ( $map['prop'] as $id => $prop ) {
 
-					foreach ( $map['prop'] as $id => $prop ) {
+					if ( !empty( $prop ) ) {
 
-						if ( !empty( $prop ) ) {
-
-							$$type[ $prop ] = $entry[ $id ];
-
-						}
+						$$type[ $prop ] = $entry[ $id ];
 
 					}
 
-				} else {
-
-					$$type[ $map['prop'] ] = $entry[ $gf_id ];
-
 				}
+
+			} else {
+
+				$$type[ $map['prop'] ] = $entry[ $gf_id ];
 
 			}
 
 		}
 
+	}
 
-		// Update customer info with fields mapped to this form
-		if ( update_customer( $entry[ $event['id_field'] ], $entry[ $event['email_field'] ], $customer ) ) {
 
-			// Create an event with mapped fields
-			customer_event( $entry[ $event['id_field'] ], $event['event_name'], $event_data );
+	// Update customer info with fields mapped to this form
+	if ( update_customer( $entry[ $event['id_field'] ], $entry[ $event['email_field'] ], $customer ) ) {
 
-		}
+		// Create an event with mapped fields
+		customer_event( $entry[ $event['id_field'] ], $event['event_name'], $event_data );
 
 	}
 
