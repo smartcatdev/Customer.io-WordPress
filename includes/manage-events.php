2b5e5356f29336dde0be627882f7989ea3177a8d
Eric Green <ericgreen.developer@gmail.com>
2017-08-02 11:23:13 -0400

    Stor non array maps in single prop

--- includes/manage-events.php
+++ includes/manage-events.php
@@ -511,7 +511,7 @@ function do_event_edit_page() {
                                         <?php
 
                                         $args = array(
-                                            'name'  => "fields[{$field['id']}]",
+                                            'name'  => "fields[{$field['id']}][prop]",
                                             'value' => ! empty( $event['field_map'][ $field['id'] ]['prop'] )
                                                 ? $event['field_map'][ $field['id'] ]['prop']
                                                 : '',
